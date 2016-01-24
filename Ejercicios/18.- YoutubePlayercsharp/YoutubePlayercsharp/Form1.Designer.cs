384a3f56ffa2b9a8254eb300231f73b54fc95dce
oscaruh <dimit28@gmail.com>
2016-01-24 11:01:45 -0600

    Player Youtube c#

---
+++ Ejercicios/18.- YoutubePlayercsharp/YoutubePlayercsharp/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace YoutubePlayercsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgListaVideos = new System.Windows.Forms.DataGridView();
+            this.PlayerYoutube = new System.Windows.Forms.WebBrowser();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Imagen = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgListaVideos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgListaVideos
+            // 
+            this.dtgListaVideos.AllowUserToAddRows = false;
+            this.dtgListaVideos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgListaVideos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Imagen,
+            this.Nombre});
+            this.dtgListaVideos.Location = new System.Drawing.Point(12, 298);
+            this.dtgListaVideos.Name = "dtgListaVideos";
+            this.dtgListaVideos.RowTemplate.Height = 50;
+            this.dtgListaVideos.Size = new System.Drawing.Size(529, 152);
+            this.dtgListaVideos.TabIndex = 0;
+            this.dtgListaVideos.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dtgListaVideos_CellMouseClick);
+            // 
+            // PlayerYoutube
+            // 
+            this.PlayerYoutube.Location = new System.Drawing.Point(12, 12);
+            this.PlayerYoutube.MinimumSize = new System.Drawing.Size(20, 20);
+            this.PlayerYoutube.Name = "PlayerYoutube";
+            this.PlayerYoutube.Size = new System.Drawing.Size(529, 262);
+            this.PlayerYoutube.TabIndex = 1;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.Visible = false;
+            // 
+            // Imagen
+            // 
+            this.Imagen.HeaderText = "Imagen";
+            this.Imagen.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.Imagen.Name = "Imagen";
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 69;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 462);
+            this.Controls.Add(this.PlayerYoutube);
+            this.Controls.Add(this.dtgListaVideos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgListaVideos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgListaVideos;
+        private System.Windows.Forms.WebBrowser PlayerYoutube;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewImageColumn Imagen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+
+    }
+}
+
---
+++ Ejercicios/18.- YoutubePlayercsharp/YoutubePlayercsharp/Form1.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubePlayercsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var json = new WebClient().DownloadString("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCne4X8czEkhh8GPRjXBIQJw&maxResults=25&key=AIzaSyADavf_BB9pkn2rrEv7kQZRLXDU5D5QK8A");
+            
+            ApiYoutube obj = JsonConvert.DeserializeObject<ApiYoutube>(json);
+
+            int total = ((ICollection)obj.items).Count;
+            
+            PlayerYoutube.Url = new System.Uri("https://www.youtube.com/embed/" + obj.items[0].id.videoId, System.UriKind.Absolute);
+
+
+            for (int i = 0; i < total; i++)
+            {
+
+                dtgListaVideos.Rows.Add(obj.items[i].id.videoId,GetImageFromUrl(obj.items[i].snippet.thumbnails.medium.url), obj.items[i].snippet.title);
+
+            }
+
+
+        }
+        public static Image GetImageFromUrl(string url)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            using (HttpWebResponse httpWebReponse = (HttpWebResponse)httpWebRequest.GetResponse())
+            {
+                using (Stream stream = httpWebReponse.GetResponseStream())
+                {
+                    return Image.FromStream(stream);
+                }
+            }
+        }
+
+        private void dtgListaVideos_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int indexVideo = e.RowIndex;
+            PlayerYoutube.Url = new System.Uri("https://www.youtube.com/embed/" + dtgListaVideos.Rows[e.RowIndex].Cells[0].Value.ToString() + "?autoplay=1", System.UriKind.Absolute);
+        }
+
+    }
+}
