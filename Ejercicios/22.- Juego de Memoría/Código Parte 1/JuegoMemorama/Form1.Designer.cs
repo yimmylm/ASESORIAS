00977995d15ab01c440225bd75592e1d6da4cf03
oscaruh <dimit28@gmail.com>
2016-03-16 23:12:23 -0600

    Juego Memorama

---
+++ Ejercicios/22.- Juego de Memoría/Código Parte 1/JuegoMemorama/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace JuegoMemorama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PanelJuego = new System.Windows.Forms.Panel();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRecord = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // PanelJuego
+            // 
+            this.PanelJuego.Location = new System.Drawing.Point(12, 12);
+            this.PanelJuego.Name = "PanelJuego";
+            this.PanelJuego.Size = new System.Drawing.Size(450, 450);
+            this.PanelJuego.TabIndex = 0;
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(468, 160);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(176, 23);
+            this.btnReiniciar.TabIndex = 1;
+            this.btnReiniciar.Text = "Reinicio de juego";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(480, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Record:";
+            // 
+            // lblRecord
+            // 
+            this.lblRecord.AutoSize = true;
+            this.lblRecord.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecord.Location = new System.Drawing.Point(563, 38);
+            this.lblRecord.Name = "lblRecord";
+            this.lblRecord.Size = new System.Drawing.Size(20, 24);
+            this.lblRecord.TabIndex = 3;
+            this.lblRecord.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 478);
+            this.Controls.Add(this.lblRecord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.PanelJuego);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelJuego;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRecord;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Ejercicios/22.- Juego de Memoría/Código Parte 1/JuegoMemorama/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoMemorama
+{
+    public partial class Form1 : Form
+    {
+        int TamanioColumnasFilas = 4;
+        int Movimientos = 0;
+        int CantidadDeCartasVolteadas = 0;
+        List<string> CartasEnumeradas;
+        List<string> CartasRevueltas;
+        ArrayList CartasSeleccionadas;
+        PictureBox CartaTemporal1;
+        PictureBox CartaTemporal2;
+        int CartaActual = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            iniciarJuego();
+        }
+        public void iniciarJuego() {
+            timer1.Enabled = false;
+            timer1.Stop();
+            lblRecord.Text = "0";
+            CantidadDeCartasVolteadas = 0;
+            Movimientos = 0;
+            PanelJuego.Controls.Clear();
+            CartasEnumeradas= new List<string>();
+            CartasRevueltas = new List<string>();
+            CartasSeleccionadas = new ArrayList();
+            for (int i = 0; i < 8; i++) {
+                CartasEnumeradas.Add(i.ToString());
+                CartasEnumeradas.Add(i.ToString());
+            }
+            var NumeroAleatorio = new Random();
+            var Resultado = CartasEnumeradas.OrderBy(item=> NumeroAleatorio.Next());
+            foreach(string ValorCarta in Resultado){
+                CartasRevueltas.Add(ValorCarta);
+            }
+            var tablaPanel = new TableLayoutPanel();
+            tablaPanel.RowCount = TamanioColumnasFilas;
+            tablaPanel.ColumnCount = TamanioColumnasFilas;
+            for (int i = 0; i < TamanioColumnasFilas;i++ )
+            {
+                var Porcentaje = 150f / (float)TamanioColumnasFilas - 10;
+                tablaPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent,Porcentaje));
+                tablaPanel.RowStyles.Add(new RowStyle(SizeType.Percent,Porcentaje));
+             }
+            int contadorFichas = 1;
+
+            for (var i = 0; i < TamanioColumnasFilas; i++)
+            {
+                for (var j = 0; j < TamanioColumnasFilas; j++)
+                {
+                    var CartasJuego = new PictureBox();
+                    CartasJuego.Name = string.Format("{0}", contadorFichas);
+                    CartasJuego.Dock = DockStyle.Fill;
+                    CartasJuego.SizeMode = PictureBoxSizeMode.StretchImage;
+                    CartasJuego.Image = Properties.Resources.Girada;
+                    CartasJuego.Cursor = Cursors.Hand;
+                    tablaPanel.Controls.Add(CartasJuego, j, i);
+                    contadorFichas++;
+                }
+            }
+            tablaPanel.Dock = DockStyle.Fill;
+            PanelJuego.Controls.Add(tablaPanel);
+
+        
+        }
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
