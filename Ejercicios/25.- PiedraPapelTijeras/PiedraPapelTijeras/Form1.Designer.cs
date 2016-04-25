9d63280811b80f3993e1a0426a0dd439bf81ee64
oscaruh <dimit28@gmail.com>
2016-04-25 10:10:25 -0500

    Piedra Papel o Tijera

---
+++ Ejercicios/25.- PiedraPapelTijeras/PiedraPapelTijeras/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace PiedraPapelTijeras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.rojo = new System.Windows.Forms.PictureBox();
+            this.empate = new System.Windows.Forms.PictureBox();
+            this.azul = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPuntos = new System.Windows.Forms.Label();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rojo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.azul)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::PiedraPapelTijeras.Properties.Resources._2;
+            this.pictureBox1.Location = new System.Drawing.Point(-60, 143);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(265, 142);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::PiedraPapelTijeras.Properties.Resources._2_d;
+            this.pictureBox2.Location = new System.Drawing.Point(211, 143);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(265, 142);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // rojo
+            // 
+            this.rojo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rojo.Image = global::PiedraPapelTijeras.Properties.Resources.ROJO_GANA;
+            this.rojo.Location = new System.Drawing.Point(1, 372);
+            this.rojo.Name = "rojo";
+            this.rojo.Size = new System.Drawing.Size(140, 76);
+            this.rojo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rojo.TabIndex = 2;
+            this.rojo.TabStop = false;
+            this.rojo.Click += new System.EventHandler(this.Verificar);
+            // 
+            // empate
+            // 
+            this.empate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.empate.Image = global::PiedraPapelTijeras.Properties.Resources.EMPATE;
+            this.empate.Location = new System.Drawing.Point(146, 372);
+            this.empate.Name = "empate";
+            this.empate.Size = new System.Drawing.Size(140, 76);
+            this.empate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.empate.TabIndex = 3;
+            this.empate.TabStop = false;
+            this.empate.Click += new System.EventHandler(this.Verificar);
+            // 
+            // azul
+            // 
+            this.azul.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.azul.Image = global::PiedraPapelTijeras.Properties.Resources.AZULGANA;
+            this.azul.Location = new System.Drawing.Point(288, 372);
+            this.azul.Name = "azul";
+            this.azul.Size = new System.Drawing.Size(140, 76);
+            this.azul.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.azul.TabIndex = 4;
+            this.azul.TabStop = false;
+            this.azul.Click += new System.EventHandler(this.Verificar);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label2.Location = new System.Drawing.Point(-2, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 42);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Puntos";
+            // 
+            // lblPuntos
+            // 
+            this.lblPuntos.AutoSize = true;
+            this.lblPuntos.BackColor = System.Drawing.Color.Transparent;
+            this.lblPuntos.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntos.ForeColor = System.Drawing.Color.FloralWhite;
+            this.lblPuntos.Location = new System.Drawing.Point(45, 60);
+            this.lblPuntos.Name = "lblPuntos";
+            this.lblPuntos.Size = new System.Drawing.Size(43, 42);
+            this.lblPuntos.TabIndex = 8;
+            this.lblPuntos.Text = "0";
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTiempo.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTiempo.ForeColor = System.Drawing.Color.FloralWhite;
+            this.lblTiempo.Location = new System.Drawing.Point(300, 25);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(33, 35);
+            this.lblTiempo.TabIndex = 9;
+            this.lblTiempo.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PiedraPapelTijeras.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(429, 446);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.lblPuntos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.azul);
+            this.Controls.Add(this.empate);
+            this.Controls.Add(this.rojo);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rojo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.azul)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox rojo;
+        private System.Windows.Forms.PictureBox empate;
+        private System.Windows.Forms.PictureBox azul;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPuntos;
+        private System.Windows.Forms.Label lblTiempo;
+    }
+}
+
---
+++ Ejercicios/25.- PiedraPapelTijeras/PiedraPapelTijeras/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PiedraPapelTijeras
+{
+    public partial class Form1 : Form
+    {
+        int PosicionOriginalPlayer1, PosicionOriginalPlayer2;
+        int movimientosManos, TiempoSeleccion, TiempoTotalJuego = 0;
+        bool MovimientoArriba; String GaneActual = "empate";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PosicionOriginalPlayer1 = pictureBox1.Location.Y;
+            PosicionOriginalPlayer2 = pictureBox2.Location.Y;
+            timer3.Enabled = true;
+            timer3.Start();
+            IniciarJuego();
+            Sonido("readygo");
+
+        }
+        public void IniciarJuego() {
+            MovimientoArriba = false;
+            movimientosManos = 0;
+            TiempoSeleccion = 150;
+            GaneActual = "empate";
+
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer1.Start();
+            timer2.Start();
+        }
+
+        public void Sonido(String NombreSonido) {
+            var sonido = (Stream)Properties.Resources.ResourceManager.GetObject(NombreSonido);
+            SoundPlayer SonidoCargado = new SoundPlayer(sonido);
+            SonidoCargado.Play();
+
+        
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int posicion = pictureBox1.Location.Y;
+            int velocidadMovimiento = 30;
+
+            if ((posicion < 281) && (MovimientoArriba == false))
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + velocidadMovimiento);
+                pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - velocidadMovimiento);
+                if (posicion == 263) { MovimientoArriba = true; }
+
+            }
+            if ((posicion > 59) && (MovimientoArriba == true))
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y-velocidadMovimiento);
+                pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y+velocidadMovimiento);
+                if (posicion == 83) { MovimientoArriba = false; movimientosManos++;}
+
+            }
+            if(movimientosManos==2){
+                timer1.Stop();
+                pictureBox1.Location = new Point(pictureBox1.Location.X, PosicionOriginalPlayer1);
+                pictureBox2.Location = new Point(pictureBox2.Location.X, PosicionOriginalPlayer2);
+
+                Random rnd = new Random();
+                int player1 = rnd.Next(1, 4);
+                int player2 = rnd.Next(1, 4);
+
+                pictureBox1.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject(player1.ToString());
+                pictureBox2.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject(player2 + "_d");
+                timer2.Start();
+                rojo.Enabled = true; azul.Enabled = true; empate.Enabled = true;
+
+                //1.- Tijera
+                //2.- Piedra
+                //3.- Papel
+
+                if (((player1 == 1) && (player2 == 2)) || ((player1 == 2) && (player2 == 3)) || ((player1 == 3) && (player2 == 1)))
+                {
+                    GaneActual = "azul";
+                }
+                if (((player1 == 1) && (player2 == 3)) || ((player1 == 2) && (player2 == 1)) || ((player1 == 3) && (player2 == 2)))
+                {
+                    GaneActual = "rojo";
+                }
+
+
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            TiempoSeleccion--;
+            lblTiempo.Text = TiempoSeleccion.ToString() + " ms";
+            if(TiempoSeleccion<=0){
+                timer2.Stop();
+                IniciarJuego();
+                Sonido("timeover");
+            }
+
+        }
+
+        private void Verificar(object sender, EventArgs e)
+        {
+            String Presionado = ((PictureBox)sender).Name;
+            if ((Presionado == GaneActual) && (TiempoSeleccion > 0)) {
+                lblPuntos.Text = (Convert.ToInt32(lblPuntos.Text) + 1).ToString();
+                timer2.Stop();
+                IniciarJuego();
+                Sonido("Punto");
+            }
+            else {
+
+                timer2.Stop();
+                IniciarJuego();
+                Sonido("error");
+            }
+            rojo.Enabled = false; azul.Enabled = false; empate.Enabled = false;
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (TiempoTotalJuego == (1000 * 1)) {
+
+                timer1.Stop();
+                timer2.Stop();
+                timer3.Stop();
+                Sonido("gameover");
+                DialogResult result = MessageBox.Show("Tiempo agotado - Puntaje Final:" + lblPuntos.Text, "Tiempo agotado", MessageBoxButtons.OK);
+                if (result == DialogResult.OK) {
+
+                    lblPuntos.Text = "0";
+                    timer3.Enabled = true;
+                    timer3.Start();
+                    IniciarJuego();
+                    Sonido("readygo");
+                
+                }
+            }
+            TiempoTotalJuego++;
+        }
+    }
+}
