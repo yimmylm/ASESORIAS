fa8459668446db84521cae514769ce2fee190e7b
oscaruh <dimit28@gmail.com>
2016-05-04 23:02:01 -0500

    Juego Marmota

---
+++ Ejercicios/26.- JuegoMarmota/JuegoMarmota/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace JuegoMarmota
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelJuego = new System.Windows.Forms.TableLayoutPanel();
+            this.pbxatrapame = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPuntos = new System.Windows.Forms.Label();
+            this.lblFallas = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxatrapame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelJuego
+            // 
+            this.panelJuego.BackColor = System.Drawing.Color.Transparent;
+            this.panelJuego.ColumnCount = 3;
+            this.panelJuego.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.panelJuego.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.panelJuego.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 109F));
+            this.panelJuego.Location = new System.Drawing.Point(139, 114);
+            this.panelJuego.Name = "panelJuego";
+            this.panelJuego.RowCount = 3;
+            this.panelJuego.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.panelJuego.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.panelJuego.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.panelJuego.Size = new System.Drawing.Size(331, 263);
+            this.panelJuego.TabIndex = 0;
+            // 
+            // pbxatrapame
+            // 
+            this.pbxatrapame.BackColor = System.Drawing.Color.Transparent;
+            this.pbxatrapame.Image = global::JuegoMarmota.Properties.Resources.a1;
+            this.pbxatrapame.Location = new System.Drawing.Point(476, 167);
+            this.pbxatrapame.Name = "pbxatrapame";
+            this.pbxatrapame.Size = new System.Drawing.Size(132, 130);
+            this.pbxatrapame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxatrapame.TabIndex = 1;
+            this.pbxatrapame.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(2, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Puntos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(2, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 39);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Fallas";
+            // 
+            // lblPuntos
+            // 
+            this.lblPuntos.AutoSize = true;
+            this.lblPuntos.BackColor = System.Drawing.Color.Transparent;
+            this.lblPuntos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPuntos.Location = new System.Drawing.Point(51, 190);
+            this.lblPuntos.Name = "lblPuntos";
+            this.lblPuntos.Size = new System.Drawing.Size(30, 31);
+            this.lblPuntos.TabIndex = 8;
+            this.lblPuntos.Text = "0";
+            // 
+            // lblFallas
+            // 
+            this.lblFallas.AutoSize = true;
+            this.lblFallas.BackColor = System.Drawing.Color.Transparent;
+            this.lblFallas.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFallas.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblFallas.Location = new System.Drawing.Point(51, 300);
+            this.lblFallas.Name = "lblFallas";
+            this.lblFallas.Size = new System.Drawing.Size(30, 31);
+            this.lblFallas.TabIndex = 9;
+            this.lblFallas.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 700;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 50000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::JuegoMarmota.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(620, 378);
+            this.Controls.Add(this.lblFallas);
+            this.Controls.Add(this.lblPuntos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbxatrapame);
+            this.Controls.Add(this.panelJuego);
+            this.Name = "Form1";
+            this.Text = "Aplasta a la Marmota";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxatrapame)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel panelJuego;
+        private System.Windows.Forms.PictureBox pbxatrapame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPuntos;
+        private System.Windows.Forms.Label lblFallas;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Ejercicios/26.- JuegoMarmota/JuegoMarmota/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoMarmota
+{
+    public partial class Form1 : Form
+    {
+        int NumeroMarmotaActual = 1;
+        int TiempoNivel = 20;
+        int Puntuacion = 0;
+        int Fallas = 0;
+        int LimiteFallas = 3;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+        public void IniciarJuego() {
+
+            NumeroMarmotaActual = 1;
+            TiempoNivel = 20;
+            Puntuacion = 0;
+            Fallas = 0;
+            LimiteFallas = 3;
+            lblPuntos.Text = "0";
+            lblFallas.Text = "0";
+            panelJuego.Controls.Clear();
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    var FichaJuego = new PictureBox();
+                    FichaJuego.Image = Properties.Resources.AgujeroVacio;
+                    FichaJuego.Name = string.Format("{0}", i + "_" + j);
+                    FichaJuego.Dock = DockStyle.Fill;
+                    FichaJuego.Cursor = Cursors.Hand;
+                    FichaJuego.SizeMode = PictureBoxSizeMode.StretchImage;
+                    FichaJuego.Click += Jugar;
+                    FichaJuego.Tag = "No";
+                    panelJuego.Controls.Add(FichaJuego, j, i);
+                }
+            }
+            timer1.Start();
+            timer2.Start();
+        
+        }
+        private void Jugar(object sender, EventArgs e) {
+            var FichaSeleccionadaUsuario = (PictureBox)sender;
+            if (FichaSeleccionadaUsuario.Tag.ToString() != "No")
+            {
+
+                if (FichaSeleccionadaUsuario.Tag.ToString() == "marmota_" + NumeroMarmotaActual)
+                {
+                    Sonido("bien");
+                    Puntuacion++;
+                    lblPuntos.Text = Puntuacion.ToString();
+                    timer1.Interval = timer1.Interval - TiempoNivel;
+                }
+                else
+                {
+                    
+                    Fallas++;
+                    lblFallas.Text = Fallas.ToString();
+                    Sonido("falla");
+                }
+
+
+            }
+            else {
+                Sonido("error");
+            
+            }
+
+        
+        }
+        public void Sonido(String NombreSonido)
+        {
+            var sonido = (Stream)Properties.Resources.ResourceManager.GetObject(NombreSonido);
+            SoundPlayer SonidoCargado = new SoundPlayer(sonido);
+            SonidoCargado.Play();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IniciarJuego();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    PictureBox pbOcultar = this.Controls.Find(i + "_" + j, true).FirstOrDefault() as PictureBox;
+                    pbOcultar.Image = Properties.Resources.AgujeroVacio;
+                    pbOcultar.Tag = "No";
+                }
+            }
+            
+            int rndColor = rnd.Next(1, 5);
+            int rndi = rnd.Next(0, 3);
+            int rndj = rnd.Next(0, 3);
+
+            PictureBox pbx = this.Controls.Find(rndi + "_" + rndj, true).FirstOrDefault() as PictureBox;
+            pbx.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("marmota_" + rndColor);
+           
+            pbx.Tag = "marmota_" + rndColor;
+
+            if (Fallas == LimiteFallas)
+            {
+                timer1.Stop();
+                MessageBox.Show("Juego terminado puntos = " + lblPuntos.Text);
+                timer2.Stop();
+                IniciarJuego();
+            }
+
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int NumeroMarmota = rnd.Next(1,5);
+            NumeroMarmotaActual = NumeroMarmota;
+            pbxatrapame.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("a" + NumeroMarmotaActual);
+        }
+
+    }
+}
