706d1f82e99764273baf02621a29a6de2774d9e1
oscaruh <dimit28@gmail.com>
2016-08-28 15:32:32 -0500

    Ejercicio 27

---
+++ Ejercicios/27.- Capturar Pantalla/CaputarPantalla/Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace CaputarPantalla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Ejercicios/27.- Capturar Pantalla/CaputarPantalla/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaputarPantalla
+{
+
+    public partial class Form1 : Form
+    {
+        Point posicionVentana;
+        bool mover = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Opacity = 0.2;
+            this.TransparencyKey = Color.Turquoise;
+            this.ControlBox = false;
+            this.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.C)) { CaputurarPantalla(); }
+            if ((e.KeyCode == Keys.M)) { this.WindowState = FormWindowState.Minimized; }
+            if ((e.KeyCode == Keys.L)) { this.WindowState = FormWindowState.Maximized; }
+            if ((e.KeyCode == Keys.Escape)) { this.Close(); }
+
+        }
+        public void CaputurarPantalla() {
+            try {
+                Bitmap CapturaBitmap = new Bitmap(this.Width, this.Height, PixelFormat.Format32bppArgb);
+                Rectangle capturaRectangulo = Screen.AllScreens[0].Bounds;
+                Graphics CapturaGrafico = Graphics.FromImage(CapturaBitmap);
+                CapturaGrafico.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, capturaRectangulo.Size);
+                Clipboard.SetImage(CapturaBitmap);
+                MessageBox.Show("Región capturada");
+            
+            }
+            catch(Exception ex){
+                MessageBox.Show(ex.Message);
+            }
+        
+        
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            posicionVentana = new Point(Cursor.Position.X - Location.X, Cursor.Position.Y - Location.Y);
+            mover = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mover = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(mover){
+                this.Location = new Point(Cursor.Position.X-posicionVentana.X,Cursor.Position.Y-posicionVentana.Y);
+            }
+        }
+
+    }
+}
